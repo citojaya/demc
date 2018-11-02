8943b29295b93a097ef88bb7e549fa799dfa10f0
Cito <citojaya@gmail.com>
2018-11-03 04:50:26 +1000

    updating from macbook

--- common.h
+++ common.h
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "double_math.h"
 
 /*--------------Common definitions---------------*/
 #define NUM_MAT 2    // two types material
