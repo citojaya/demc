2aae771cfda2a90c409ac4dbab8d4de88ae68bcb
Cito <citojaya@gmail.com>
2018-11-08 11:25:30 +1100

    updating from macbook

--- mathop.c
+++ mathop.c
@@ -2,38 +2,56 @@
 
 void insertionSort(double *nbArray, int size, int *parIArray, int *cellSEArray) {
 
-   double nbValueToInsert;
-   int pIValue, cSEValue; 
+   double nbValToInsert;
+   int pIValToInsert, cSEValToInsert; 
    int holePosition;
    int i;
   
    // loop through all numbers 
    for(i = 1; i < size; i++) { 
-	
-      // select a value to be inserted. 
-      nbValueToInsert = nbArray[i];
-      pIValue = parIArray[i];
-      // select the hole position where number is to be inserted 
-      holePosition = i;
+        // select a value to be inserted. 
+        nbValToInsert = nbArray[i];
+        pIValToInsert = parIArray[i];
+        cSEValToInsert = cellSEArray[i];
+        // select the hole position where number is to be inserted 
+        holePosition = i;
 		
       // check if previous no. is larger than value to be inserted 
-      while (holePosition > 0 && nbArray[holePosition-1] > nbValueToInsert) {
-         nbArray[holePosition] = nbArray[holePosition-1];
-         parIArray[holePosition] = parIArray[holePosition-1];
-         cellSEArray[holePosition] = cellSEArray[holePosition-1];
-         holePosition--;
-         printf(" item moved : %lf\n" , nbArray[holePosition]);
-      }
+    while (holePosition > 0 && nbArray[holePosition-1] > nbValToInsert) {
+        nbArray[holePosition] = nbArray[holePosition-1];
+        parIArray[holePosition] = parIArray[holePosition-1];
+        cellSEArray[holePosition] = cellSEArray[holePosition-1];
+        holePosition--;
 
-      if(holePosition != i) {
-         printf(" item inserted : %lf, at position : %d\n" , nbValueToInsert,holePosition);
+        /*bmbn => bnbm neighbourlist unchanged
+          bmen => enbm remove from neighbourlist
+          emen => enem neighbourlist unchanged
+          embn => bnem add to neighbourlist */
+        //check for begining position of particle
+        if(cellSEArray[holePosition] == 1){
+            if(cellSEArray[holePosition-1] == 2){
+                printf("removed\n");
+                //remove from neighbourlist
+            }
+        }
+        //check for end position of particle
+        else if(cellSEArray[holePosition] == 2){
+            if(cellSEArray[holePosition-1] == 1){
+                printf("added\n");
+                //add to neighbourlist
+            }
+        }
+         //printf(" item moved : %lf\n" , nbArray[holePosition]);
+    }
+
+    if(holePosition != i) {
+         //printf(" item inserted : %lf, at position : %d\n" , nbValToInsert,holePosition);
          // insert the number at hole position 
-         nbArray[holePosition] = nbValueToInsert;
-         parIArray[holePosition] = pIValue;
-         cellSEArray[holePosition] = cSEValue;
-      }
-
-      printf("Iteration %d#:",i);		
-   }  
+         nbArray[holePosition] = nbValToInsert;
+         parIArray[holePosition] = pIValToInsert;
+         cellSEArray[holePosition] = cSEValToInsert;
+    }
+      //printf("Iteration %d#:",i);		
+    }  
 }
 
